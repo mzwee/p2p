5ae638be1aa0185af17adc22b23e0396424095cf
mzwee <mzwee95@gmail.com>
2015-09-20 02:01:26 -0400

    added navbar

---
+++ app/scripts/controllers/navbar.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name p2pApp.controller:NavCtrl
+ * @description
+ * # NavCtrl
+ * Controller of the p2pApp
+ */
+angular.module('p2pApp')
+  .controller('NavCtrl', function ($scope) {
+  	
+  });
