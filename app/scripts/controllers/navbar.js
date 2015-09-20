67d3241249a9675ef8cef3a0d569719b229209e1
mzwee <mzwee95@gmail.com>
2015-09-20 02:52:13 -0400

    added sign out functionality

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -8,6 +8,15 @@
  * Controller of the p2pApp
  */
 angular.module('p2pApp')
-  .controller('NavCtrl', function ($scope) {
-  	
+  .controller('NavCtrl', function ($scope) {    
+    $scope.isSignedIn = function() {
+      if(Parse.User.current()) {
+        $scope.username = Parse.User.current().getUsername();
+        return true;
+      }
+      return false;
+    }
+    $scope.logoutBtnClick = function() {
+      Parse.User.logOut();
+    };
   });
